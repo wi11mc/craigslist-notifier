c1e32d04f8b114899222f270bfdc79ea5c898c12
l3omc2 <wj7@hawaii.edu>
2024-11-11 08:18:05 -0800

    need to add and test the pushbullet api key

--- scraper.py
+++ scraper.py
@@ -11,19 +11,40 @@ logging.basicConfig(level=logging.INFO)
 API_KEY = ""
 pb = Pushbullet(API_KEY)
 
-PreviousFirstPostTitle = None
+PreviousTitle = None
 
-def sendNoti(title):
-    pb.push_note(title)
+def sendNoti(title, freestuff):
+    pb.push_note(title, freestuff)
     logging.info("sent noti")
 
 def theScraper():
-    url = "https://honolulu.craigslist.org/search/big/zip#search=1~gallery~0~0"
+    global PreviousTitle
+    url = "https://honolulu.craigslist.org/search/big/zip#search=1~list~0~0"
+    
     response = requests.get(url)
     
     if response.status_code == 200:
         logging.info(f"successful scrape from {url}")
+        soup = BeautifulSoup(response.text, 'html.parser')
+    
+        FirstTitle = soup.find('li', class_='cl-search-result')
+    
+        if FirstTitle:
+            NewPost = FirstTitle.find('a', class_='posting-title').text.strip()
+        
+            if PreviousTitle is None or NewPost != PreviousTitle:
+                logging.info(f"free stuff detected {NewPost}")
+                sendNoti("free stuff", NewPost)
+                PreviousTitle = NewPost
+            else:
+                logging.info("no new posts")
+        else:
+            logging.error("cant find first post.")
+        
     else:
         logging.error(f"scrape failed at {url}")
-        return []
     
+    
+while True:
+    theScraper()
+    time.sleep(600)
